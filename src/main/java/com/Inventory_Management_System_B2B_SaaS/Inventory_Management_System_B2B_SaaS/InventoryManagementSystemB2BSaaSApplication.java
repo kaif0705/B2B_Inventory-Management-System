cf4376dcbb8673e0c2d790fb645d53d1cfb13a2f
kaif0705 <mohammedkaifkilledar5@gmail.com>
2025-09-16 20:24:42 +0530

    Started Developing B2B Inventory Management System

---
+++ src/main/java/com/Inventory_Management_System_B2B_SaaS/Inventory_Management_System_B2B_SaaS/InventoryManagementSystemB2BSaaSApplication.java
@@ -0,0 +1,13 @@
+package com.Inventory_Management_System_B2B_SaaS.Inventory_Management_System_B2B_SaaS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementSystemB2BSaaSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementSystemB2BSaaSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Inventory_Management_System_B2B_SaaS/Inventory_Management_System_B2B_SaaS/InventoryManagementSystemB2BSaaSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Inventory_Management_System_B2B_SaaS.Inventory_Management_System_B2B_SaaS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemB2BSaaSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
